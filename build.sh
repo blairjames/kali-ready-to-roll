b6db50f85cd719cc9be3cce082955a80159d1e4f
Blair <blair@blairjames.com>
2020-10-02 20:24:58 +1000

    Automatic build

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-docker build . -t blairy/kali_patched || exit 1
+docker build . -t blairy/kali_patched || echo 'Docker Build Failed!' 
+
+git="/usr/bin/git -C /home/docker/kali_patched_docker"
+
+$git pull git@github.com:blairjames/kali_patched_docker.git || echo 'Pull Failed!'
+$git add --all || echo 'Add Failed!'
+$git commit -a -m 'Automatic build '$timestp || echo 'Commit Failed!'
+$git push || echo 'Push Failed!'
 
