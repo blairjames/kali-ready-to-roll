8b03ba4e6fc4f8a96d4d11132f50237821fb7290
Blair <blair@blairjames.com>
2021-01-04 23:04:38 +1000

    Automatic build

--- build.sh
+++ build.sh
@@ -16,3 +16,8 @@ $git pull git@github.com:blairjames/kali_patched_docker.git || echo 'Pull Failed
 $git add --all || echo 'Add Failed!'
 $git commit -a -m 'Automatic build '$timestp || echo 'Commit Failed!'
 $git push || echo 'Push Failed!'
+
+# Prune
+/usr/bin/git gc --prune
+
+
