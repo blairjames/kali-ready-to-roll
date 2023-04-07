e5700c1ba17813508f6f22de11d314d638fd9929
Canis <32350627+blairjames@users.noreply.github.com>
2023-04-07 19:32:35 +1000

    Refactor - configuration vars now set once in main

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ except() {
 configure() {
   local path
   local image_name
-  path="${USERHOME}"/docker/kali
-  image_name=docker.io/blairy/kali-ready-to-roll
+  path="$1"
+  image_name="$2"
   readonly path
   readonly image_name
   build "${path}" "${image_name}" || except "Docker Build Failed, see journalctl and build.log for more information."
@@ -26,11 +26,25 @@ build() {
   image_name="$2"
   readonly path
   readonly image_name
-  sudo docker build --pull -t "${image_name}":latest "${path}" 2>&1 | tee "${path}"/build.log 
+  docker build --pull -t "${image_name}" "${path}" 2>&1 | tee "${path}"/build.log 
+}
+
+push() {
+  local name
+  name="$1"
+  readonly name
+  docker push "${name}"
 }
 
 main() {
-  configure || except "Failed to configure the image name and path." 
+  local path
+  local name
+  path="${USERHOME}"/docker/kali
+  name="docker.io/blairy/kali-ready-to-roll"
+  readonly path
+  readonly name
+  configure "${path}" "${name}" || except "Failed to configure the image name and path." 
+  push "${name}" || except "Failed to push image to Docker hub"
 }
 
 main "$@"
