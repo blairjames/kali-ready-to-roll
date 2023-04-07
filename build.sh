47c90021a15ad39b6bd90afd2e556da684ea9b67
Canis <32350627+blairjames@users.noreply.github.com>
2023-04-07 12:09:24 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env bash
 
+configure() {
+  local -r path
+  local -r image_name
+  path=/$HOME/docker/kali
+  image_name=docker.io/blairy/kali-ready-to-roll
+}
+
 build() {
-  local path;
-  path=/$HOME/docker/kali;
-  sudo docker build --pull -t docker.io/blairy/kali-ready-to-roll:latest $path --progress=plain 2>&1 | tee $path/build.log;
+  local path
+  local image_name
+  path=$1
+  image_name=$2
+  readonly path
+  readonly image_name
+  sudo docker build --pull -t $image_name:latest $path --progress=plain 2>&1 | tee $path/build.log
+}
+
+except() {
+  local message
+  message=$1
+  readonly message
+  printf "Error! - " + $message
+    + "\nBuild process will now exit."
+  exit 1
 }
 
 main() {
-  build || exit 111;
+  configure || except "Failed to configure the image name and path." 
+  build || except "Docker Build Failed, see journalctl and build.log for more information."
 }
 
-main;
+main
